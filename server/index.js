37d0b724e8fcfe52f9aa85be438b9b637a59006b
Ryan Garren <rryangarren@gmail.com>
2022-01-12 08:33:40 -0700

    completed step 3

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+const port = 3001;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
