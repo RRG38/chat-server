4cd1195b2d08b1581156bd48b1bfba8669751b85
Ryan Garren <rryangarren@gmail.com>
2022-01-12 09:15:35 -0700

    completed step 4

---
+++ server/controllers/messages_controller.js
@@ -0,0 +1,33 @@
+let messages = [];
+let id = 0;
+
+class Message {
+  constructor(id, text, time) {
+    (this.id = id), (this.text = text), (this.time = time);
+  }
+}
+
+module.exports = {
+  read: (req, res, next) => {
+    res.status(200).send(messages);
+  },
+  create: (req, res, next) => {
+    const { text, time } = req.body;
+    const newMessage = new Message(id, text, time);
+    id++;
+    messages.push(newMessage);
+  },
+  update: (req, res, next) => {
+    const { id } = req.params;
+    const { text } = req.body;
+    const index = messages.findIndex((e) => e.id === +id);
+    messages[index].text = text;
+    res.status(200).send(messages);
+  },
+  delete: (req, res, next) => {
+    const { id } = req.params;
+    const index = messages.findIndex((e) => e.id === +id);
+    messages.splice(index, 1);
+    res.status(200).send(messages);
+  },
+};
